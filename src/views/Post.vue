736bf2d58f7652853d866832e17cfc3e276803f9
hadikp <hp20001025@gmail.com>
2022-12-01 21:39:18 +0100

    post form

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -4,11 +4,70 @@
 </script>
 
 <template>
-  <main v-if="userData.user.id">
+  <main>
      <h1>Üdvözöllek {{ userData.user.userName }} </h1>
-    <p>Lorem,</p>
+    <form class="post-form" action="">
+        <input class="post-title" type="text" placeholder="Bejegyzés címe" v-model="title">
+        <textarea class="post-content" rows="13" placeholder="Bejegyzés tartalma" v-model="content"></textarea>
+      <div class="buttons">
+        <button class="buttons-cancel">mégse</button>
+        <button class="buttons-save">mentés</button>
+      </div>
+    </form>
 </main>
 </template>
 
 <style scoped>
+
+.buttons {
+  
+    margin: 1rem;
+  }
+
+  .buttons-cancel, .buttons-save {
+    background-color: #ff2b75;
+    box-shadow: 0 6px 20px -5px rgba(0,0,0,0.4);
+    border: none;
+    border-radius: 10px;
+    color: white;
+    cursor: pointer;
+    font-family:'Gill Sans', 'Gill Sans MT';
+    padding: 0.4rem 0.8rem;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 1.5rem;
+    margin: 0.1rem 0.1rem;
+  }
+
+  .buttons-save {
+    margin-left: 2rem;
+    background-color: orange;
+  }
+
+  
+
+  h1 {
+    font-size: 2rem;
+    margin-top: 1rem;
+    text-align: center;
+  }
+
+  .post-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+  }
+
+  .post-title, .post-content {
+    color: #000;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    margin: 0.5rem;
+    padding: 0.1rem;
+    width: 90%;
+  }
+
+
 </style>
